352a4986fca4f2e9d84736d7da86fbe82e24aeae
robert <robert@vulcan.local>
2010-01-27 02:00:00 +0100

    remove boost/gil from testapp

--- testapp/main.cpp
+++ testapp/main.cpp
@@ -8,18 +8,11 @@
 #include <vil/vil_copy.h>
 #include <vil/vil_image_view.h>
 
-#include <boost/gil/gil_all.hpp>
-#include <boost/gil/image.hpp>
-#include <boost/gil/typedefs.hpp>
-#include <boost/gil/color_convert.hpp>
-#include <boost/gil/pixel.hpp>
-
 #include "image_view.h"
+#include "mem_image_view_r.h"
 
 #include <typeinfo>
 #include <limits>
-
-using namespace boost::gil;
 
 int main()
 {
@@ -59,37 +52,9 @@ int main()
     << pixel.B() << ", "
     << pixel.A() << vcl_endl;
 
-    //////////////////////////////
-    // GIL stuff
-
-    // create GIL image
-    vcl_cerr << "Creating GIL image." << vcl_endl;
-    rgba32_image_t gil_img(width, height);
-    rgba32_view_t gil_view = gil_img._view;
-
-    // get a pointer and create parasitic VIL view
-    vil_rgba<channel_t>* gil_ptr = reinterpret_cast<vil_rgba<channel_t>*>(
-                                       interleaved_view_get_raw_data(gil_img._view)
-                                   );
-    vil_image_view<vil_rgba<channel_t> > transfer_view(gil_ptr, width, height, 1, 1, width, 1);
-
-    vcl_cerr << "Copying data to GIL image." << vcl_endl;
-    transfer_view.deep_copy(img_view);
-
-    vcl_cerr << gil_view(100, 100)[0] << ", "
-    << gil_view(100, 100)[1] << ", "
-    << gil_view(100, 100)[2] << ", "
-    << gil_view(100, 100)[3] << vcl_endl;
-
-    typedef boost::gil::pixel<boost::gil::channel_type<rgba32_image_t>::type, gray_layout_t> gray_pixel_t;
-    typedef color_converted_view_type<rgba32_view_t, gray_pixel_t>::type gray_conv_view_t;
-
-    const gray_conv_view_t& gray_conv_view = boost::gil::color_converted_view<gray_pixel_t> (gil_view);
-
-    vcl_cerr << gray_conv_view.width() << "," << gray_conv_view.height() << vcl_endl;
-    vcl_cerr << (uint32_t) gray_conv_view(100, 100)[0] << vcl_endl;
-
 //    tiff_read_image("/home/robert/kombi1.tif",img);
+
+    phtr::MemImageViewR phtr_mem_view(0, width, height);
 
     return 0;
 }
