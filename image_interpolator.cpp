1bcce7d4d741ed672cd34337692f149a32d0de9e
robert <robert@Vulcan>
2010-01-29 02:00:00 +0100

    preliminary interpolator skeleton

---
+++ image_interpolator.cpp
@@ -0,0 +1,28 @@
+#include "image_interpolator.h"
+
+namespace phtr
+{
+
+    ImageInterpolator::ImageInterpolator(IImageViewR* image_view)
+            : image_view_(image_view)
+    {
+        scale_x_ = static_cast<interp_coord_t>(image_view_->width());
+        scale_y_ = static_cast<interp_coord_t>(image_view_->height());
+    }
+
+    ImageInterpolator::~ImageInterpolator()
+    {
+        //NIL
+    }
+
+    channel_t ImageInterpolator::get_px_val_r(interp_coord_t x, interp_coord_t y)
+    {
+        interp_coord_t x_scaled = (x + 0.5) * scale_x_;
+        interp_coord_t y_scaled = (y + 0.5) * scale_y_;
+
+        // FIXME
+        return image_view_->get_px_val_r(x_scaled, y_scaled);
+    }
+
+
+} // namespace phtr
---
+++ include/image_interpolator.h
@@ -0,0 +1,32 @@
+#ifndef __IMAGEINTER_POLATOR_H__
+#define __IMAGEINTER_POLATOR_H__
+
+#include "image_view.h"
+#include "types.h"
+
+namespace phtr
+{
+
+    class ImageInterpolator
+    {
+        public:
+            ImageInterpolator(IImageViewR* image_view);
+            virtual ~ImageInterpolator();
+
+        public:
+            channel_t get_px_val_r(interp_coord_t x, interp_coord_t y);
+
+        private:
+            IImageViewR* image_view_;
+
+        private:
+            channel_t scale_x_;
+
+        private:
+            channel_t scale_y_;
+
+    }; // class ImageInterpolator
+
+} // namespace phtr
+
+#endif // __IMAGEINTER_POLATOR_H__
