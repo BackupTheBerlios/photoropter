ac4277345fcd8fc1c500e718dfa3ce1ee791f4ac
robert <robert@Vulcan>
2010-02-08 02:00:00 +0100

    new demo application

--- testapp/phtrtest2.cpp
+++ testapp/phtrtest2.cpp
@@ -1,3 +1,17 @@
+/*
+*
+* Sample program to demonstrate basic usage of Photoropter.
+*
+* (c) 2010 by Robert Fendt
+*
+*/
+
+// Boost is used for parsing of program options.
+#include <boost/program_options.hpp>
+#include <boost/tokenizer.hpp>
+namespace po = boost::program_options;
+
+// VIL is needed for image file I/O
 #include <vcl_iostream.h>
 #include <vxl_config.h>
 #include <vil/vil_rgb.h>
@@ -8,6 +22,7 @@
 #include <vil/vil_copy.h>
 #include <vil/vil_image_view.h>
 
+// Now the Photoropter stuff
 #include <photoropter/types.h>
 #include <photoropter/mem_image_view_r.h>
 #include <photoropter/mem_image_view_w.h>
@@ -19,8 +34,236 @@
 #include <photoropter/geom_correction_model.h>
 
 #include <ctime>
+#include <string>
+#include <vector>
+#include <sstream>
 
-int main()
+struct Parameters
 {
-  return 0;
+    Parameters()
+            : ptlens_corr(false),
+            ptlens_params(5, 0),
+            param_aspect(0),
+            param_crop(1.0),
+            image_crop(1.0)
+    {}
+
+    bool ptlens_corr;
+    std::vector<double> ptlens_params;
+    double param_aspect;
+    double param_crop;
+    double image_crop;
+    std::string inp_file;
+    std::string outp_file;
+};
+
+bool parse_command_line(int argc, char* argv[], Parameters& params)
+{
+
+    try
+    {
+
+        po::options_description opt_desc("Allowed options");
+        opt_desc.add_options()
+        ("help", "show options")
+        ("ptlens", po::value<std::string>(), "Set PTLens correction model parameters: a;b;c;x0;y0")
+        ("param-aspect", po::value<double>(), "Aspect ratio used for parameter calibration")
+        ("param-crop", po::value<double>(), "Crop factor used for parameter calibration")
+        ("image-crop", po::value<double>(), "Diagonal image crop factor")
+        ("input-file", po::value<std::string>(), "Input file")
+        ("output-file", po::value<std::string>(), "Output file");
+
+        po::positional_options_description pos_opts;
+        pos_opts.add("input-file", 1);
+        pos_opts.add("output-file", 1);
+
+        po::variables_map options_map;
+        po::store(po::command_line_parser(argc, argv).
+                  options(opt_desc).positional(pos_opts).run(), options_map);
+        po::notify(options_map);
+
+        if (options_map.count("help"))
+        {
+            std::cerr << opt_desc << "\n";
+            return false;
+        }
+
+        if (options_map.count("param-crop"))
+        {
+            params.param_crop = options_map["param-crop"].as<double>();
+        }
+
+        if (options_map.count("param-aspect"))
+        {
+            params.param_aspect = options_map["param-aspect"].as<double>();
+        }
+
+        if (options_map.count("image-crop"))
+        {
+            params.image_crop = options_map["image-crop"].as<double>();
+        }
+
+        if (options_map.count("input-file"))
+        {
+            params.inp_file = options_map["input-file"].as<std::string>();
+        }
+        else
+        {
+            std::cerr << "Error: no input file given" << std::endl;
+            return false;
+        }
+
+        if (options_map.count("output-file"))
+        {
+            params.outp_file = options_map["output-file"].as<std::string>();
+        }
+        else
+        {
+            std::cerr << "Error: no output file given" << std::endl;
+            return false;
+        }
+
+        if (options_map.count("ptlens"))
+        {
+            params.ptlens_corr = true;
+
+            typedef boost::tokenizer<boost::char_separator<char> > tokenizer_t;
+            typedef boost::char_separator<char> separator_t;
+
+            std::string param_string = options_map["ptlens"].as<std::string>();
+
+            separator_t sep(":;");
+            tokenizer_t tokens(param_string, sep);
+
+            size_t param_idx(0);
+            for (tokenizer_t::const_iterator it = tokens.begin(); it != tokens.end(); ++it)
+            {
+                std::stringstream sstr(*it);
+                sstr >> params.ptlens_params[param_idx++];
+            }
+
+            if (not(param_idx == 3 or param_idx == 5))
+            {
+                std::cerr << "Error: incorrent number of parameters for PTLens correction" << std::endl;
+                return false;
+            }
+
+            std::cerr << "Performing PTLens correction: "
+                      << params.ptlens_params[0] << ":"
+                      << params.ptlens_params[1] << ":"
+                      << params.ptlens_params[2] << ":"
+                      << params.ptlens_params[3] << ":"
+                      << params.ptlens_params[4] << std::endl;
+        }
+
+    }
+    catch (po::invalid_command_line_syntax& e)
+    {
+        std::cerr << "Error: " << e.what() << std::endl;
+        return false;
+    }
+
+    return true;
+}
+
+void convert_8bit(const Parameters& params)
+{
+    std::cerr << "Performing 8bit conversion." << std::endl;
+
+    using namespace phtr;
+
+    // typedefs and constants
+    // the Photoropter storage type
+    const Storage::type storage_type(Storage::rgb_8_inter);
+    // the equivalent vil type
+    typedef vxl_uint_8 vil_channel_t;
+
+    typedef ImageBuffer<storage_type> buffer_t;
+    typedef MemImageViewR<storage_type> view_r_t;
+    typedef MemImageViewW<storage_type> view_w_t;
+    typedef view_w_t::iter_t iter_t;
+    typedef ImageInterpolator<Interpolation::bilinear, view_r_t> interp_t;
+    typedef ImageTransform<interp_t, view_w_t, 2> transform_t;
+
+    // load the input image
+    std::cerr << "Loading test image." << std::endl;
+    vil_image_view<vil_rgb<vil_channel_t> > loaded_img =
+        vil_convert_to_component_order(
+            vil_convert_to_n_planes(
+                3, vil_convert_stretch_range(
+                    vil_channel_t(), vil_load(params.inp_file.c_str())
+                )
+            )
+        );
+
+    // image size
+    size_t img_width = loaded_img.ni();
+    size_t img_height = loaded_img.nj();
+
+    // create image buffers
+    std::cerr << "Creating buffers for Photoropter." << std::endl;
+    // input buffer
+    buffer_t src_img_buf(img_width, img_height);
+    view_r_t src_img_view(src_img_buf.data(), img_width, img_height);
+    // output buffer
+    buffer_t dst_img_buf(img_width, img_height, true);
+    view_w_t dst_img_view(dst_img_buf.data(), img_width, img_height);
+
+    // attach VIL views for I/O
+    vil_image_view<vil_rgb<vil_channel_t> > vil_src_view
+    (static_cast<vil_rgb<vil_channel_t>*>(src_img_buf.data()), img_width, img_height, 1, 1, img_width, 1);
+    vil_image_view<vil_rgb<vil_channel_t> > vil_dst_view
+    (static_cast<vil_rgb<vil_channel_t>*>(dst_img_buf.data()), img_width, img_height, 1, 1, img_width, 1);
+
+    // copy data to input buffer
+    std::cerr << "Copying image to buffer." << std::endl;
+    vil_src_view.deep_copy(loaded_img);
+
+    // image transformation object
+    transform_t transform(src_img_view, dst_img_view);
+
+    // add correction models
+    double image_aspect = src_img_view.aspect_ratio();
+    if (params.ptlens_corr)
+    {
+        PTLensGeomModel ptlens_mod(params.param_aspect,
+                                   image_aspect,
+                                   params.param_crop,
+                                   params.image_crop);
+        ptlens_mod.set_model_params(params.ptlens_params[0],
+                                    params.ptlens_params[1],
+                                    params.ptlens_params[2],
+                                    1.0,
+                                    params.ptlens_params[3],
+                                    params.ptlens_params[4]);
+
+        transform.geom_queue().add_model(ptlens_mod);
+    }
+
+    // perform transformation
+    time_t t0 = time(0);
+    std::cerr << "Transforming." << std::endl;
+    transform.do_transform();
+    time_t t1 = time(0);
+
+    std::cerr << "Time taken: " << difftime(t1, t0) << " seconds." << std::endl;
+
+    std::cerr << "Saving output image." << std::endl;
+    vil_save(vil_dst_view, params.outp_file.c_str());
+}
+
+int main(int argc, char* argv[])
+{
+    Parameters params;
+
+    if (!parse_command_line(argc, argv, params))
+    {
+        return 1;
+    }
+    else
+    {
+        convert_8bit(params);
+    }
+
+    return 0;
 }
